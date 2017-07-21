947eb14e0ef7029b7fee2a4624460b867ab1b3cc
rickxie <xiemengcheng@outlook.com>
2017-07-21 10:22:44 +0800

    MailExtension

---
+++ SimpleMailSender.EmailExtension/EmailServerConfig.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace SimpleMailSender.EmailExtension
+{
+    [Serializable]
+    public class EmailServerConfig
+    {
+        public int Port; //发送邮件所用的端口号（htmp协议默认为25）
+        public string HostAddress { get; set; }//发件箱的邮件服务器地址（IP形式或字符串形式均可）
+        public string SenderDisplayName { get; set; }//发件箱的用户名（即@符号前面的字符串，例如：hello@163.com，用户名为：hello）
+        public string ServerEmail { get; set; } //发件箱的用户名（即@符号前面的字符串，例如：hello@163.com，用户名为：hello）
+        public string ServerPassword { get; set; } //发件箱的密码
+        public string ServerUserName { get; set; } //发件箱的用户名（即@符号前面的字符串，例如：hello@163.com，用户名为：hello）
+        public bool EnableSsl { get; set; } //是否对邮件内容进行socket层加密传输
+        public bool EnablePwdAuthentication { get; set; } = true; //是否对发件人邮箱进行密码验证
+        public int Timeout { get; set; } = 30000;
+        public bool UseDefaultCredentials { get; set; } = false;
+        public string Subject { get; set; }  
+        public string Content { get; set; } 
+        public string To { get; set; }
+        public bool ShowDialog { get; set; } = true;
+        public string Error { get; set; }
+        public EmailServerConfig()
+        {}
+
+        /// <summary>
+        /// 消息提醒
+        /// </summary>
+        [System.Xml.Serialization.XmlIgnore]
+        public Action<string> Notice { get; set; } = s => { }; 
+
+        /// <summary>
+        /// 发送服务器配置
+        /// </summary>
+        /// <param name="hostAddress"></param>
+        /// <param name="hostAccount"></param>
+        /// <param name="hostPassword"></param>
+        /// <param name="hostPort"></param>
+        /// <param name="enableSsl"></param>
+        /// <param name="senderEmail"></param>
+        /// <param name="senderName"></param>
+        /// <param name="subject"></param>
+        /// <param name="content"></param>
+        /// <param name="toList"></param>
+        /// <param name="timeout"></param>
+        public EmailServerConfig(
+            string hostAddress, 
+            string hostAccount,  
+            string hostPassword, 
+            int hostPort, 
+            bool enableSsl, 
+            string senderEmail, 
+            string senderName, 
+            string subject,
+            string content,
+            string toList,
+            int timeout = 30000)
+        {
+            HostAddress = hostAddress;
+            ServerUserName = hostAccount;
+            ServerEmail = senderEmail;
+            ServerPassword = hostPassword;
+            Port = hostPort;
+            EnableSsl = enableSsl;
+            SenderDisplayName = senderName;
+            Timeout = timeout;
+            Subject = subject;
+            Content = content;
+            To = toList;
+        }
+
+        /// <summary>
+        /// 设置并发送
+        /// </summary>
+        public bool Send()
+        {
+            EmailSender helper = new EmailSender(HostAddress, ServerUserName, ServerEmail, ServerPassword, Port, EnableSsl, SenderDisplayName, UseDefaultCredentials, Timeout);
+            try
+            {
+                helper.Send(Subject, Content, To.Split(','));
+                if (ShowDialog)
+                    Notice("发送成功");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if(ShowDialog)
+                    Notice(ex.ToString());
+                Error = ex.ToString();
+                return false;
+            }
+        }
+
+        public EmailServerConfig DeepCopy()
+        {
+            return MemberwiseClone() as EmailServerConfig;
+        }
+     }
+
+    
+}
---
+++ SimpleMailSender.EmailExtension/XMLSerializor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SimpleMailSender.EmailExtension
+{
+    public static class XmlSerializor
+    {
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public static string XmlSerialize<T>(this T entity)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (TextWriter writer = new StringWriter(buffer))
+            {
+                serializer.Serialize(writer, entity);
+            }
+
+            return buffer.ToString();
+        }
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xmlString"></param>
+        /// <returns></returns>
+        public static T XmlDeSerialize<T>(this string xmlString)
+        {
+            T cloneObject = default(T);
+
+            StringBuilder buffer = new StringBuilder();
+            buffer.Append(xmlString);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            using (TextReader reader = new StringReader(buffer.ToString()))
+            {
+                Object obj = serializer.Deserialize(reader);
+                cloneObject = (T)obj;
+            }
+            return cloneObject;
+        }
+    }
+}
